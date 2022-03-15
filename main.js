3840d761ccea44d63c061cc3be074bf3424067b7
10xyz <82887902+10xyz-coder@users.noreply.github.com>
2022-03-15 20:46:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ function clickpic()
     document.getElementById("profilepic").innerHTML = "assets/COVIDICON.png";
   }
 }
+
+function spider_game()
+{
+  var elem = document.getElementById("spider_game")
+  document.getElementById("spider_game").style.display = "inline"
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+function spider_game_hide()
+{
+  document.getElementById("spider_game").style.display = "none"
+}
